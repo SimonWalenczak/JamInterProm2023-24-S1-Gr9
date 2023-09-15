4a08594a05b36074aa649fab66f1914d14284463
Sporicide <114909361+sSporicide@users.noreply.github.com>
2023-09-15 11:38:43 +0200

    ScrollWheelScript
    DOMMAGE

--- Assets/Scripts/ScrollWheelManager/ScrollWheelManager.cs
+++ Assets/Scripts/ScrollWheelManager/ScrollWheelManager.cs
@@ -1,30 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScrollWheelManager : MonoBehaviour
 {
     public Interactable interactable;
-
-    public List<Sprite> ModelForms;
-    public List<Sprite> PlayerForms;
-
-    public List<SpriteRenderer> ModelsNumber;
+    public TextMeshPro displayText;
     public List<int> ActualNumber;
-
     public List<Scrollbar> ScrollWheel;
+    public Image imageToUpdate;
+    public Sprite[] sprites;
+    public GameObject ButtonScreenRotating;
 
+    private int targetValue;
     private int nbSuccess;
     private bool isLaunching;
-
-    public void StartWheeling()
-    {
-        foreach (var modelSprite in ModelsNumber)
-        {
-            modelSprite.sprite = ModelForms[Random.Range(0, 9)];
-        }
-
-    }
 
     private void Update()
     {
@@ -42,28 +33,38 @@ public class ScrollWheelManager : MonoBehaviour
                 scrollwheel.interactable = true;
             }
         }
-        for (int i = 0; i < ScrollWheel.Count; i++)
-        {
-            int scrollValue = Mathf.Clamp(Mathf.RoundToInt(ScrollWheel[i].value * 9), 0, 9);
-            ActualNumber[i] = scrollValue;
-        }
+
+        nbSuccess = 0;
 
         for (int i = 0; i < ScrollWheel.Count; i++)
         {
-            if (ModelsNumber[i].sprite == PlayerForms[ActualNumber[i]])
+            int scrollValue = Mathf.Clamp(Mathf.RoundToInt(ScrollWheel[i].value * 9), 0, 9);
+            displayText.text = " : " + scrollValue;
+            ActualNumber[i] = scrollValue;
+
+            if (scrollValue < sprites.Length)
+            {
+                imageToUpdate.sprite = sprites[scrollValue];
+                ButtonScreenRotating.transform.Rotate(new Vector3(0f, 0f, 10));
+            }
+
+            if (scrollValue == targetValue)
             {
                 nbSuccess++;
             }
         }
 
-        if (nbSuccess == 1)
+        if (nbSuccess == ScrollWheel.Count)
         {
             isLaunching = false;
             StartWheeling();
             interactable.ResetScreen();
         }
+    }
 
-        nbSuccess = 0;
+    public void StartWheeling()
+    {
+        targetValue = Random.Range(0, 10);
     }
 
     private void Start()
