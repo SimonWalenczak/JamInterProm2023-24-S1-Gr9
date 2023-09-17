4b312dfd6ff679d77a31120c476dce002ea161ec
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2023-09-17 15:28:22 +0200

    Scoring setup

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameData
+{
+    public static int BestScore;
+    public static int LastestScore;
+    public static int ActualScore;
+}
