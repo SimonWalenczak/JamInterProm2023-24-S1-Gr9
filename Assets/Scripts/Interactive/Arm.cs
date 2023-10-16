1acb9dd4f4d540ea36da08c6d4abe62838e9c2fe
RemyMasselot <masselotremy@gmail.com>
2023-10-16 19:03:32 +0200

    Modif enviro + Gestion des mains

--- Assets/Scripts/Interactive/Arm.cs
+++ Assets/Scripts/Interactive/Arm.cs
@@ -17,7 +17,7 @@ public class Arm : MonoBehaviour
     [Space(10)] [SerializeField] private bool LeftArm;
     [SerializeField] private Vector3 _leftOrigin;
     [SerializeField] private Vector3 _rightOrigin;
-    [SerializeField, Range(0,1)] private float _limit;
+    [SerializeField, Range(0, 1)] private float _limit;
 
     public bool IsActualArm;
 
@@ -26,7 +26,7 @@ public class Arm : MonoBehaviour
     [SerializeField] private float LimitY;
 
     private float actualMousePosY;
-    
+
     private void Start()
     {
         if (!LeftArm)
@@ -53,64 +53,66 @@ public class Arm : MonoBehaviour
         {
             actualMousePosY = mousePosition.y;
         }
-        
-        if (LeftArm)
-        {
-            if (IsActualArm)
-            {
-                if (Input.mousePosition.x <= Screen.width * _limit)
-                {
-                    _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
-                        transform.position.z);
-                }
-                else
-                {
-                    IsActualArm = false;
-                    otherArm.IsActualArm = true;
-                }
-            }
-            else
-            {
-                if (Input.mousePosition.x < Screen.width * 0.01f)
-                {
-                    _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
-                        transform.position.z);
-                }
-                else
-                {
-                    _targetPosition = _leftOrigin;
-                }
-            }
-        }
-        else
-        {
-            if (IsActualArm)
-            {
-                if (Input.mousePosition.x >= Screen.width * (1- _limit))
-                {
-                    _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
-                        transform.position.z);
-                }
-                else
-                {
-                    IsActualArm = false;
-                    otherArm.IsActualArm = true;
-                }
-            }
-            else
-            {
-                if (Input.mousePosition.x > Screen.width * 1)
-                {
-                    _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
-                        transform.position.z);
-                }
-                else
-                {
-                    _targetPosition = _rightOrigin;
-                }
-            }
-        }
 
+        //if (LeftArm)
+        //{
+        //    if (IsActualArm)
+        //    {
+        //        if (Input.mousePosition.x <= Screen.width * _limit)
+        //        {
+        //            _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
+        //                transform.position.z);
+        //        }
+        //        else
+        //        {
+        //            IsActualArm = false;
+        //            otherArm.IsActualArm = true;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (Input.mousePosition.x < Screen.width * 0.01f)
+        //        {
+        //            _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
+        //                transform.position.z);
+        //        }
+        //        else
+        //        {
+        //            _targetPosition = _leftOrigin;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    if (IsActualArm)
+        //    {
+        //        if (Input.mousePosition.x >= Screen.width * (1- _limit))
+        //        {
+        //            _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
+        //                transform.position.z);
+        //        }
+        //        else
+        //        {
+        //            IsActualArm = false;
+        //            otherArm.IsActualArm = true;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (Input.mousePosition.x > Screen.width * 1)
+        //        {
+        //            _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
+        //                transform.position.z);
+        //        }
+        //        else
+        //        {
+        //            _targetPosition = _rightOrigin;
+        //        }
+        //    }
+        //}
+
+        _targetPosition = new Vector3(mousePosition.x + offsetX, actualMousePosY + offsetY,
+                    transform.position.z);
         transform.position = Vector3.Lerp(transform.position, _targetPosition, lerpSpeed * Time.deltaTime);
     }
 }
